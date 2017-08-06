cee26e4f4a84c7c00f67b067066bed13fe32d4f8
Bex Way <rebeccaway2016@u.northwestern.edu>
2017-08-06 13:40:55 -0500

    finished submit button functinoality, survey processing, and survey result modal

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,23 @@
 module.exports = friendsList = [
   {
   "name": "friend",
-  "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+  "photo": "http://lorempixel.com/300/300/",
+  "scores": [
+    "5",
+    "1",
+    "4",
+    "4",
+    "5",
+    "1",
+    "2",
+    "5",
+    "4",
+    "1"
+    ]
+  },
+  {
+  "name": "friend2",
+  "photo": "http://lorempixel.com/300/300/",
   "scores": [
     "5",
     "1",
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,5 +7,38 @@ module.exports = function(app){
 
   app.post("/api/friends", function(req, res) {
     //put code for adding to friendslist here
+    var newFriend = req.body;
+    console.log(newFriend);
+
+    // calculate score
+    foundFriend = findNearestFriend(newFriend);
+
+    friendsList.push(newFriend);
+    res.json(foundFriend);
+
   });
 };
+
+
+function findNearestFriend(friend){
+  var minDiff = 500;
+  var bestFriendList = [];
+  for(var f in friendsList){
+    comparisonFriend = friendsList[f];
+    var diff = 0;
+    for(var i=0; i<friend.scores.length; i++){
+      diff += Math.abs(parseInt(friend.scores[i])-parseInt(comparisonFriend.scores[i]));
+    }
+    //If it's less, that becomes the best friend, and the min difference changes
+    if(diff<minDiff){
+      bestFriendList = [comparisonFriend];
+      minDiff = diff;
+    }
+    //if the scores match, store with friends of matching score
+    else if(diff === minDiff){
+      bestFriendList.push(comparisonFriend);
+    }
+  }
+  //return a random friend of the best score; this is to avoid favoring friends who are early in the friendslist array.
+  return bestFriendList[Math.floor(Math.random()*bestFriendList.length)];
+}
